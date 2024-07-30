2564263693d0f2c35cf343139c2afe7ab1060f6f
Nathan Welliver <nwelliver.564@gmail.com>
2024-07-30 15:34:06 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 13, 'M', 'Brown', 'Chill', 1);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Rowdy', 24, 'M', 'Tan', 'Mild', 1);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Tabitha', 16, 'F', 'Black', 'Friendly', 1);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 33, 'M', 'Brown', 'Temper', 1);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Melissa', 17, 'F', 'Tan', 'None', 0);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Grinch', 44, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Black', 'Friendly', 0);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'Tan', 'Whinny', 1);
